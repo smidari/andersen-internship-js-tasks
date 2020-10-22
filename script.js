2d7f034ef331036e18015341493c764997e3d7cd
Dasha <smidariaa@gmail.com>
2020-10-22 14:30:29 +0300

    Done the task 3

---
+++ script.js
@@ -0,0 +1,29 @@
+function Person(name) {
+  const a = { name };
+  a.getName = function () {
+    return a.name;
+  };
+  return a;
+}
+
+function Man(name, facialHair) {
+  const superA = Person(name);
+  superA.facialHair = facialHair;
+
+  superA.getName = function () {
+    return `Name: ${superA.name}`;
+  };
+
+  superA.getFacialHair = function () {
+    return superA.facialHair;
+  };
+
+  return superA;
+}
+
+const person = Person('somebody');
+console.log(person.getName()); // somebody
+
+const man = Man('Viktor', true);
+console.log(man.getName()); // Name: Viktor
+console.log(man.getFacialHair()); // true
