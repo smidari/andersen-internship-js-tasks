4b975d5ed428e15109437d326d73fb398391f150
Dasha <smidariaa@gmail.com>
2020-10-22 10:16:48 +0300

    Changed the solution to the task

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 // 1 Singleton (практика Замыканий)
-let singleton = {
-  size: 100,
-  getSize() {
-    return this.size;
-  },
-
-  setSize(size) {
-    this.size = size;
-  },
-};
 function Universe() {
-  return singleton;
+  let instance = null;
+  if (Universe.instance) {
+    return Universe.instance;
+  } else {
+    this.size = 100;
+    this.getSize = function () {
+      return this.size;
+    };
+    this.setSize = function (size) {
+      this.size = size;
+    };
+    Universe.instance = this;
+    return Universe.instance;
+  }
 }
 
 let a = new Universe();
-
 let b = new Universe();
 
 console.log(a === b); // должны получить true
-
 console.log(a.getSize(), b.getSize()); // 100, 100
 a.setSize(200);
 console.log(a.getSize(), b.getSize()); // 200, 200
