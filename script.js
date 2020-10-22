597d5f97af455970578a180180c7027df75ce524
Dasha <smidariaa@gmail.com>
2020-10-22 14:17:06 +0300

    Done task 2

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-function Person (name)  {
+function Person(name) {
   this.name = name;
-  this.setName = function () {
-    this.name = name;
-  };
-  this.getName = function () {
-    return this.name
-  };
 }
 
-function Man (name, facialHair) {
+Person.prototype.setName = function () {
+  this.name = name;
+};
+Person.prototype.getName = function () {
+  return this.name;
+};
+
+function Man(name, facialHair) {
   Person.call(this, name);
   this.facialHair = facialHair;
   this.getFacialHair = function () {
     return this.facialHair;
   };
   this.getName = function () {
-    return 'Name: '+ this.name
+    return "Name: " + this.name;
   };
 }
 
-const person = new Person('somebody');
+Man.prototype = Object.create(Person.prototype);
+Man.prototype.constructor = Man;
+
+const person = new Person("somebody");
 console.log(person.getName()); // somebody
 
-const man = new Man('Viktor', true);
+const man = new Man("Viktor", true);
 console.log(man.getName()); // Name: Viktor
 console.log(man.getFacialHair()); // true
